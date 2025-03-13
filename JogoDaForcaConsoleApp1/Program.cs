6b71f277d21f1a23eb62a3660c1fb0972874dd4c
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-13 14:36:51 -0300

    commit!

--- JogoDaForcaConsoleApp1/Program.cs
+++ JogoDaForcaConsoleApp1/Program.cs
@@ -4,7 +4,130 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] palavras = {
+                    "ABACATE",
+                    "ABACAXI",
+                    "ACEROLA",
+                    "ACAI",
+                    "ARACA",
+                    "ABACATE",
+                    "BACABA",
+                    "BACURI",
+                    "BANANA",
+                    "CAJA",
+                    "CAJU",
+                    "CARAMBOLA",
+                    "CUPUACU",
+                    "GRAVIOLA",
+                    "GOIABA",
+                    "JABUTICABA",
+                    "JENIPAPO",
+                    "MACA",
+                    "MANGABA",
+                    "MANGA",
+                    "MARACUJA",
+                    "MURICI",
+                    "PEQUI",
+                    "PITANGA",
+                    "PITAYA",
+                    "SAPOTI",
+                    "TANGERINA",
+                    "UMBU",
+                    "UVA",
+                    "UVAIA"
+                };
+
+            Random geradordenumeros = new Random();
+
+            int indiceescolhido = geradordenumeros.Next(palavras.Length);
+            string palavraEscolhida = palavras[indiceescolhido];
+
+            char[] letrasEncontradas = new char[palavraEscolhida.Length];
+
+            for(int CaractereAtual = 0; CaractereAtual < letrasEncontradas.Length; CaractereAtual++)
+            {
+                letrasEncontradas[CaractereAtual] = '_';
+            }
+
+                int QuantidadeDeErros = 0;
+                bool JogadorAcertou = false;
+                bool JogadorPerdeu = false;
+
+            do
+            {
+                string cabecadodesenho = QuantidadeDeErros >= 1 ? "o" : "";
+                string troncododesenho = QuantidadeDeErros >= 2 ? "x" : "";
+                string troncoinferiordodesenho = QuantidadeDeErros >= 2 ? " x " : "";
+                string bracoesquerdododesenho = QuantidadeDeErros >= 3 ? "/" : "";
+                string bracodireitododesenho = QuantidadeDeErros >= 4 ? "\\" : "";
+                string pernasdodesenho = QuantidadeDeErros >= 5 ? "/ \\" : "";
+                Console.Clear();
+               
+                string LetrasEncontradasCompleta = string.Join(" ", letrasEncontradas);
+          
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine(" ___________        ");
+                Console.WriteLine(" |/        |        ");
+                Console.WriteLine(" |         {0}      ",cabecadodesenho);
+                Console.WriteLine(" |        {0}{1}{2} ",bracoesquerdododesenho,troncododesenho,bracodireitododesenho);
+                Console.WriteLine(" |        {0}       ",troncoinferiordodesenho);
+                Console.WriteLine(" |        {0}       ",pernasdodesenho);
+                Console.WriteLine(" |                  ");
+                Console.WriteLine(" |                  ");
+                Console.WriteLine("_|____              ");
+                Console.WriteLine("");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine($"palavra escolhida: {LetrasEncontradasCompleta}");
+                Console.WriteLine($"------------------------------");
+                Console.WriteLine($"Quantidade de erros do jogador: {QuantidadeDeErros}");
+                Console.WriteLine($"------------------------------");
+
+                Console.WriteLine("Digite um caractere");
+                string palavra = Console.ReadLine().ToUpper();
+
+                if (palavra.Length > 1)
+                {
+                    Console.WriteLine("Informe apenas um caractere");
+                    continue;
+                }
+                bool LetraFoiEncontrada = false;
+                char chute = palavra[0];
+
+                for(int ContadorDeCaracteres = 0; ContadorDeCaracteres < palavraEscolhida.Length; ContadorDeCaracteres++)
+                {
+                    char CaractereAtual = palavraEscolhida[ContadorDeCaracteres];
+
+                    if (chute == CaractereAtual) 
+                    {
+                        letrasEncontradas[ContadorDeCaracteres] = CaractereAtual;
+                        LetraFoiEncontrada = true;
+                    }                  
+                }
+                if( LetraFoiEncontrada == false)
+                {
+                    QuantidadeDeErros++;
+                }
+                string palavraEncontradaCompleta = String.Join("", letrasEncontradas);
+
+                JogadorAcertou = palavraEncontradaCompleta == palavraEscolhida;
+                JogadorPerdeu = QuantidadeDeErros > 5;
+
+                if (JogadorAcertou)
+                {
+                    Console.WriteLine($"Voce acertou a palavra escolhida {palavraEscolhida}, Parabens!");
+                    Console.ReadLine();
+                }
+                    
+                if (JogadorPerdeu)
+                {
+                    Console.WriteLine($"Voce perdeuu a palavra escolhida era: {palavraEscolhida}");
+                    Console.ReadLine();
+                }
+
+
+            } while (JogadorAcertou == false && JogadorPerdeu == false);
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# JogoDaForcaConsoleApp1
+#Jogo da Forca
+
+![](https://imgur.com/RyW9d6R)
+
+#Funcionalidades:
+- **Interação com o usuario:** Permite o usuario de tentar acertar uma palavra sorteada pelo sistema.
+- **Contagem de erros:** Consegue contar os erros do usuario.
+- **Sistema de Vitória ou Derrota:** Consegue descobrir se o usario acertou a palavra ou passou dos 5 erros permitidos.
+
+
+#Como usar
+1. Clone o repositório ou baixe o código fonte.
+2. Abra o terminal ou o prompt de comando e navegue até a pasta raiz
+3. Utilize o comando abaixo para restaurar as dependências do projeto.
+
+
+```
+dotnet restore
+```
+
+4. Em seguida, complete uma solução utilizando o comando:
+```
+dotnet build --configuration Release
+```
+
+5. Para executar o projeto compilado em tempo real:
+```
+dotnet run --- project JogoDaForca.ConsoleApp
+````
+
+6. Para executar o arquivo compilado, navegue até a pasta ./JogoDaForca.ConsoleApp/bin/Release/net8.0/e execute o arquivo:
+````
+JogoDaForca.ConsoleApp.exe
+````
+
+#Requisitos
+- .NET SDK para construção e execução do projeto.
